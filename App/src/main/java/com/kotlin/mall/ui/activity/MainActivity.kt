c2ba94417b1508b85cafc2ea46424ee2eec789ba
chenzhiy <419083145@qq.com>
2020-07-19 18:18:59 +0800

    1.更新底部菜单栏

--- App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
+++ App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
@@ -3,11 +3,26 @@ package com.kotlin.mall.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.mall.R
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBottomNavBar.checkMsgBadge(false)
+        mBottomNavBar.checkCartBadge(20)
+
+        Observable.timer(2,TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {mBottomNavBar.checkMsgBadge(true)}
+
+        Observable.timer(5,TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {mBottomNavBar.checkCartBadge(0)}
     }
 }
